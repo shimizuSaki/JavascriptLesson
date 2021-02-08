281120086d453c2f058acfc41471b70a954fb6e5
清水咲希 <shimizusaki0125@gmail.com>
2021-02-08 18:16:17 +0900

    phpファイル、インクルード機能のテスト

--- index.php
+++ index.php
@@ -1,20 +1,15 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>JavaScriptPractice</title>
-</head>
-<body>
+<?php include('./incrude/header.php') ?>
   <h1>JavaScriptPractice</h1>
   <section>
-    <h2>JSアプリ作成練習計画表</h2>
+    <h2>JSアプリ作成練習</h2>
     <p>githubのリポジトリは
       <a href="https://github.com/shimizuSaki/JavascriptLesson">こちら</a>
     </p>
+    <h2>アプリ一覧</h2>
     <ol>
       <li><a href="https://shimizusaki.github.io/JavascriptLesson/JavaScriptPractice/04_miniApp_original/01_stockChecker/index.html">stockChecker</a></li>
       <li><a href="https://shimizusaki.github.io/JavascriptLesson/JavaScriptPractice/04_miniApp_original/02_WebDice/index.html">WebDice</a></li>
+      <li><a href="https://shimizusaki.github.io/JavascriptLesson/JavaScriptPractice/04_miniApp_original/03_oximeter/index.html">oximeter</a></li>
     </ol>
   </section>
 </body>
